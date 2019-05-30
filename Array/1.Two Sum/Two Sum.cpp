071588aef0feded3c144713577ed958ea066426c
812692830@qq.com <812692830@qq.com>
2019-05-30 08:28:21 +0800

    Two Sum Solution

--- Array/1.Two Sum/Two Sum.cpp
+++ Array/1.Two Sum/Two Sum.cpp
@@ -1,3 +1,31 @@
+
+//Solution 1
+class Solution {
+public:
+	vector<int> twoSum(vector<int>& nums, int target) {
+		vector<int> results;
+
+		for (int i = 0; i < nums.size(); i++)
+		{
+			for (int j = i + 1; j < nums.size(); j++)
+			{
+				if (nums[i] + nums[j] == target)
+				{
+					results.push_back(i);
+					results.push_back(j);
+
+					return results;
+				}
+				else
+				{
+					continue;
+				}
+			}
+		}
+	}
+};
+
+//Solution 2
 class Solution {
 public:
 	vector<int> twoSum(vector<int>& nums, int target) {
