eedf6f9b2d244d21547ba1a4d43667490c77f10e
Sahil Rawat <66208562+sahilrawat001@users.noreply.github.com>
2022-07-11 11:54:15 -0700

    Update Reverse Integer.cpp

--- Math/7. Reverse Integer/Reverse Integer.cpp
+++ Math/7. Reverse Integer/Reverse Integer.cpp
@@ -31,3 +31,18 @@ public:
 		}
 	}
 };
+
+//Solution 3
+
+class Solution {
+public:
+    int reverse(int x) {
+       long   r=0;    
+         while(x!=0){
+           long rev=x%10;
+            r=r*10+rev;
+            x=x/10;  
+        }
+         return (r > INT_MAX || r < INT_MIN) ? 0 : r;
+        }   
+};
